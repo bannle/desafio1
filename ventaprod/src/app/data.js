fd80fd482514fefb024ba81285b857b5f6b8b5db
bannle <maryfloresdke@gmail.com>
2025-02-26 23:04:49 -0600

    tercer commit xD

---
+++ ventaprod/src/app/data.js
@@ -0,0 +1,142 @@
+export const data = [
+    {
+        id: 1,
+        nombre: 'Windows 11 Pro',
+        precio: 15,
+        urlimg: 'https://tech.com.sv/wp-content/uploads/2022/04/windows_11_pro_01_l.jpg',
+        cantidad: 2
+    },
+    {
+        id: 2,
+        nombre: 'Windows 11 Home',
+        precio: 12,
+        urlimg: 'https://cdn.webshopapp.com/shops/332813/files/429090302/image.jpg',
+        cantidad: 2
+    },
+    {
+        id: 3,
+        nombre: 'Visual Studio 2022 Pro',
+        precio: 140,
+        urlimg: 'https://blitzhandel24.imgbolt.de/media/image/2b/40/2b/Microsoft-Visual-Studio-2022.jpg',
+        cantidad: 1
+    },
+    {
+        id: 4,
+        nombre: 'Adobe Photoshop Elements 2024',
+        precio: 105,
+        urlimg: 'https://softwaremaz.com/wp-content/uploads/2024/08/photoshop-2024.jpg',
+        cantidad: 1
+    },
+    {
+        id: 5,
+        nombre: 'Microsoft Office 2021',
+        precio: 150,
+        urlimg: 'https://www.galactechstore.com/wp-content/uploads/2021/09/Office-2021.png',
+        cantidad: 1
+    },
+    {
+        id: 6,
+        nombre: 'CorelDRAW Graphics Suite 2023',
+        precio: 200,
+        urlimg: 'https://www.coreldraw.com/static/cdgs/images/cdgs-2023/cdgs-product-box-en.png',
+        cantidad: 1
+    },
+    {
+        id: 7,
+        nombre: 'AutoCAD 2024',
+        precio: 1600,
+        urlimg: 'https://www.autodesk.com/content/dam/autodesk/www/products/autocad/autocad-2024.png',
+        cantidad: 1
+    },
+    {
+        id: 8,
+        nombre: 'Adobe Premiere Pro 2024',
+        precio: 250,
+        urlimg: 'https://www.adobe.com/content/dam/cc/icons/premiere.svg',
+        cantidad: 1
+    },
+    {
+        id: 9,
+        nombre: 'Avast Premium Security',
+        precio: 40,
+        urlimg: 'https://static.avast.com/assets/logo/avast-logo-orange.png',
+        cantidad: 3
+    },
+    {
+        id: 10,
+        nombre: 'Kaspersky Total Security',
+        precio: 50,
+        urlimg: 'https://www.kaspersky.com/content/en-global/images/b2c/global-store/total-security.png',
+        cantidad: 2
+    },
+    {
+        id: 11,
+        nombre: 'Adobe Illustrator 2024',
+        precio: 250,
+        urlimg: 'https://www.adobe.com/content/dam/cc/icons/illustrator.svg',
+        cantidad: 1
+    },
+    {
+        id: 12,
+        nombre: 'WinRAR',
+        precio: 29,
+        urlimg: 'https://www.win-rar.com/fileadmin/templates/img/logo.png',
+        cantidad: 4
+    },
+    {
+        id: 13,
+        nombre: 'Microsoft Project 2021',
+        precio: 500,
+        urlimg: 'https://www.microsoft.com/en-us/microsoft-365/blog/wp-content/uploads/sites/2/2020/10/Project-for-the-web-logo.png',
+        cantidad: 1
+    },
+    {
+        id: 14,
+        nombre: 'Cinema 4D',
+        precio: 3500,
+        urlimg: 'https://www.maxon.net/fileadmin/images/Maxon_products/cinema-4d/c4d-logo-square-256.png',
+        cantidad: 1
+    },
+    {
+        id: 15,
+        nombre: 'VLC Media Player Pro',
+        precio: 20,
+        urlimg: 'https://www.videolan.org/images/logo.png',
+        cantidad: 3
+    },
+    {
+        id: 16,
+        nombre: 'Sony Vegas Pro 20',
+        precio: 399,
+        urlimg: 'https://softwarestore.es/wp-content/uploads/2022/09/Sony-Vegas-Pro-20.png',
+        cantidad: 1
+    },
+    {
+        id: 17,
+        nombre: 'Final Cut Pro',
+        precio: 300,
+        urlimg: 'https://www.apple.com/v/final-cut-pro/t/images/overview/hero_logo__b6lptbqhx4uu_large_2x.png',
+        cantidad: 1
+    },
+    {
+        id: 18,
+        nombre: 'ZBrush 2024',
+        precio: 895,
+        urlimg: 'https://pixologic.com/zbrush/getting-started/learn-zbrush/zb-logo.png',
+        cantidad: 1
+    },
+    {
+        id: 19,
+        nombre: 'Ableton Live 12',
+        precio: 749,
+        urlimg: 'https://ableton-production.imgix.net/uploads/education_program/image/140219/image-width_500.jpg',
+        cantidad: 1
+    },
+    {
+        id: 20,
+        nombre: 'Cubase Pro 12',
+        precio: 580,
+        urlimg: 'https://new.steinberg.net/wp-content/uploads/2021/11/Cubase_Pro-Icon.png',
+        cantidad: 1
+    }
+];

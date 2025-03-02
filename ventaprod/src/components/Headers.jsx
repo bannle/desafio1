d2bfcbad1ccc824c88d6796e457884155ec814aa
bannle <maryfloresdke@gmail.com>
2025-03-01 21:54:25 -0600

    v6.2

--- ventaprod/src/components/Headers.jsx
+++ ventaprod/src/components/Headers.jsx
@@ -1,32 +1,83 @@
-import React from "react";
-export const Headers = () => {
+"use client"
+import { useState } from "react";
+export const Headers = ({
+    allProducts,
+    setAllProducts,
+    total,
+    countProducts,
+    setCountProducts,
+    setTotal,
+}) => {
+    const [active, setActive] = useState(false);
+    const onDeleteProduct = product => {
+        if (window.confirm(`¿Estás seguro de que deseas eliminar ${product.nombre}?`)) {
+          const results = allProducts.filter(item => item.id !== product.id);
+          setAllProducts(results);
+          setTotal(total - product.precio * product.cantidad);
+          setCountProducts(countProducts - product.cantidad);
+        }
+    };
+
+    const onCleanCart = () => {
+        const confirmDelete = window.confirm("¿Estás seguro de que quieres vaciar el carrito?");
+        if (confirmDelete) {
+          setAllProducts([]);
+          setTotal(0);
+          setCountProducts(0);
+        }
+    };
     return (
         <header>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous"></link>
             <h1>Tienda de Software</h1>
             <div className="container-icon">
-                <div className="container-cart-icon">
-                    <img src="https://w7.pngwing.com/pngs/275/763/png-transparent-cart-shopping-supermarket-shoppingcart-ecommerce-e-commerce-shopping-trolley-caddy.png" alt="carrito" className="icon-cart" />
+                <div className='container-cart-icon' onClick={() => setActive(!active)}>
+                    <img src="https://w7.pngwing.com/pngs/275/763/png-transparent-cart-shopping-supermarket-shopping-cart-ecommerce-e-commerce-shopping-trolley-caddy.png" alt="carrito" className="icon-cart" />
                     <div className="count-products">
-                        <span id="contador-productos">0</span>
+                        <span id="contador-productos">{countProducts}</span>
                     </div>
                 </div>
-                <div className="container-cart-products hidden-cart">
-                    <div className="row-product hidden">
-                        <div className="cart-product">
-                            <div className="info-cart-product">
-                                <span className="cantidad-producto-carrito">1</span>
-                                <p className="titulo-producto-carrito">Don Quijote</p>
-                                <span className="precio-producto-carrito">$80</span>
+                <div className={`container-cart-products ${active ? '' : 'hidden-cart'
+                    }`}
+                >
+                    {allProducts.length ? (
+                        <>
+                            <div className='row-product'>
+                                {allProducts.map(product => (
+                                    <div className='cart-product' key={product.id}>
+                                        <div className='info-cart-product'>
+                                        <img src={product.urlimg} alt={product.nombre} className="img-product me-2" width="50" height="50" />
+                                            <p className='titulo-producto-carrito'>
+                                                 {product.nombre}
+                                            </p>
+                                            <span className='cantidad-producto-carrito'>
+                                                 x{product.cantidad}
+                                            </span>
+                                            <span className='precio-producto-carrito'>
+                                                ${product.precio}
+                                            </span>
+                                        </div>
+                                        <img src="https://static.vecteezy.com/system/resources/previews/018/887/462/original/signs-close-icon-png.png"
+                                            alt="cerrar"
+                                            className="icon-close"
+                                            onClick={() => onDeleteProduct(product)}
+                                        />
+                                    </div>
+                                ))}
                             </div>
-                            <img
-                                src="https://static.vecteezy.com/system/resources/previews/018/887/462/original/signs-close-iconpng.png"  alt="cerrar" className="icon-close" />
-                        </div>
-                    </div>
-                    <div className="cart-total hidden">
-                        <h3>Total:</h3>
-                        <span className="total-pagar">$200</span>
-                    </div>
-                    <p className="cart-empty">El carrito está vacío</p>
+
+                            <div className='cart-total'>
+                                <h3>Total:</h3>
+                                <span className='total-pagar'>${total}</span>
+                            </div>
+
+                            <button className='btn-clear-all' onClick={onCleanCart}>
+                                Vaciar Carrito
+                            </button>
+                        </>
+                    ) : (
+                        <p className='cart-empty'>El carrito está vacío</p>
+                    )}
                 </div>
             </div>
         </header>);
--- ventaprod/src/components/ProductList.jsx
+++ ventaprod/src/components/ProductList.jsx
@@ -1,20 +1,63 @@
+
 import React from "react";
 import { data } from "src/data";
-export const ProductList = () => {
-    return (
-        <div className="container-items">
-            {data.map(product => (
-                <div className="item" key={product.id}>
-                    <figure>
-                        <img src={product.urlimg} alt={product.nombre} />
-                    </figure>
-                    <div className="info-product">
-                        <h2 className="titulo-producto-carrito ">{product.nombre}</h2>
-                        <p className="price">${product.precio}</p>
-                        <button className="btn-add-cart">Añadir al carrito</button>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
-}
+export const ProductList = ({
+    allProducts,
+    setAllProducts,
+    countProducts,
+    setCountProducts,
+    total,
+    setTotal,
+}) => {
+    const onAddProduct = product => {
+        const productWithQuantity = { ...product, cantidad: 1 }; // Agregamos cantidad por defecto
+    
+        const existingProduct = allProducts.find(item => item.id === product.id);
+    
+        if (existingProduct) {
+            if (existingProduct.cantidad >= product.cantidad) {
+                alert("No hay más productos disponibles");
+                return;
+            }
+            const updatedProducts = allProducts.map(item =>
+                item.id === product.id
+                    ? { ...item, cantidad: item.cantidad + 1 }
+                    : item
+            );
+            setTotal(total + product.precio);
+            setCountProducts(countProducts + 1);
+            return setAllProducts(updatedProducts);
+        }
+    
+        if (product.cantidad <= 0) {
+            alert("Producto agotado");
+            return;
+        }
+    
+        setTotal(total + product.precio);
+        setCountProducts(countProducts + 1);
+        setAllProducts([...allProducts, productWithQuantity]);
+    };
+    
+    
+      
+    return ( 
+        <div className='container-items'> 
+        
+            {data.map(product => ( 
+                <div className='item' key={product.id}> 
+                    <figure> 
+                        <img src={product.urlimg} alt={product.nombre} /> 
+                    </figure> 
+                    <div className='info-product'> 
+                        <h2>{product.nombre}</h2> 
+                        <p className='price'>${product.precio}</p> 
+                        <button onClick={() => onAddProduct(product)}> 
+                            Añadir al carrito 
+                        </button> 
+                    </div> 
+                </div> 
+            ))} 
+        </div> 
+    ); 
+}; 
